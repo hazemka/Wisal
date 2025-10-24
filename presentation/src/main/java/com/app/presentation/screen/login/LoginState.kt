41b44916dae4d82644c732c6ffa25f12d7cd6bc3
Hazem Alkateb <hazem22-1@hotmail.com>
2025-10-24 10:24:38 +0300

    implement login screen api and events

--- presentation/src/main/java/com/app/presentation/screen/login/LoginState.kt
+++ presentation/src/main/java/com/app/presentation/screen/login/LoginState.kt
@@ -1,7 +1,11 @@
 package com.app.presentation.screen.login
 
+import com.app.presentation.utlis.StringValue
+
 data class LoginState(
     val idNumber: String = "",
     val password: String = "",
+    val idNumberError: StringValue? = null,
+    val passwordError: StringValue? = null,
     val isLoading: Boolean = false,
 )
